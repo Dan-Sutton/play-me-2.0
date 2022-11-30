180d1ae8c05f5e291cc06e932825048aa7935cca
Dan-Sutton <dewasutton@gmail.com>
2022-11-30 22:06:19 +0000

    sign out works

--- pages/artistPage.js
+++ pages/artistPage.js
@@ -10,38 +10,39 @@ function ArtistPage(props) {
   }
   const route = useRouter();
   if (!user) route.push("/auth/login");
-  return (
-    <div>
-      <div id="artist-head">
-        <h1>{`Welcome back ${user.displayName}`}</h1>
-        <p id="request-code">REQUEST CODE: 1234</p>
-      </div>
+  if (user)
+    return (
+      <div>
+        <div id="artist-head">
+          <h1>{`Welcome back ${user.displayName}`}</h1>
+          <p id="request-code">REQUEST CODE: 1234</p>
+        </div>
 
-      <div id="table">
-        <h2>Your Requests</h2>
-        <table>
-          <tr>
-            <th>Song Title</th>
-            <th>Artist Name</th>
-            <th>User</th>
-            <th>Delete</th>
-          </tr>
-        </table>
-      </div>
+        <div id="table">
+          <h2>Your Requests</h2>
+          <table>
+            <tr>
+              <th>Song Title</th>
+              <th>Artist Name</th>
+              <th>User</th>
+              <th>Delete</th>
+            </tr>
+          </table>
+        </div>
 
-      <div id="delete-all-btn">
-        <button id="delete-all">DELETE ALL</button>
-      </div>
+        <div id="delete-all-btn">
+          <button id="delete-all">DELETE ALL</button>
+        </div>
 
-      <button
-        onClick={() => {
-          auth.signOut();
-        }}
-      >
-        Sign Out
-      </button>
-    </div>
-  );
+        <button
+          onClick={() => {
+            auth.signOut();
+          }}
+        >
+          Sign Out
+        </button>
+      </div>
+    );
 }
 
 export default ArtistPage;
