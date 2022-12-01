12fb94eedb22a5680bbf9ec12f3f2e74a2dada40
Dan-Sutton <dewasutton@gmail.com>
2022-12-01 20:52:15 +0000

    can make requests

--- pages/auth/login.js
+++ pages/auth/login.js
@@ -1,9 +1,22 @@
 import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import { auth } from "../../utils/firebase";
-import React from "react";
+import React, { useState } from "react";
 import { useRouter } from "next/router";
+import {
+  addDoc,
+  collection,
+  onSnapshot,
+  query,
+  where,
+} from "firebase/firestore";
+import { db } from "../../utils/firebase";
 
 function Login(props) {
+  const [requestCode, setrequestCode] = useState();
+  const [title, setTitle] = useState();
+  const [artist, setartist] = useState();
+  const [name, setname] = useState();
+
   const route = useRouter();
   const googleProvider = new GoogleAuthProvider();
   const GoogleLogin = async () => {
@@ -15,37 +28,62 @@ function Login(props) {
     }
   };
 
+  const submitRequest = async () => {
+    const requestsCollectionRef = collection(db, "requests");
+    await addDoc(requestsCollectionRef, {
+      reqCode: parseInt(requestCode),
+      title: title,
+      artist: artist,
+      user: name,
+    });
+
+    setrequestCode("");
+    setTitle("");
+    setartist("");
+    setname("");
+  };
+
   return (
     <div>
       <h1 id="playme-title">Play Me!</h1>
 
-      <form>
-        <div class="user-input">
-          <input
-            type="text"
-            id="request-code"
-            placeholder="REQUEST CODE"
-            required
-          />
-          <input
-            type="text"
-            id="song-title"
-            placeholder="SONG TITLE"
-            required
-          />
-          <input
-            type="text"
-            id="artist-name"
-            placeholder="ARTIST NAME"
-            required
-          />
-          <input type="text" id="your-name" placeholder="YOUR NAME" />
-        </div>
+      <div class="user-input">
+        <input
+          type="number"
+          id="request-code"
+          placeholder="REQUEST CODE"
+          required
+          onChange={(e) => setrequestCode(e.target.value)}
+          value={requestCode}
+        />
+        <input
+          type="text"
+          id="song-title"
+          placeholder="SONG TITLE"
+          required
+          onChange={(e) => setTitle(e.target.value)}
+          value={title}
+        />
+        <input
+          type="text"
+          id="artist-name"
+          placeholder="ARTIST NAME"
+          required
+          onChange={(e) => setartist(e.target.value)}
+          value={artist}
+        />
+        <input
+          type="text"
+          id="your-name"
+          placeholder="YOUR NAME"
+          onChange={(e) => setname(e.target.value)}
+          value={name}
+        />
+      </div>
 
-        <div id="submit-btn">
-          <input id="submit" placeholder="SUBMIT" />
-        </div>
-      </form>
+      <div id="submit-btn">
+        <button onClick={submitRequest}>SUBMIT</button>
+      </div>
 
       <div class="login">
         <button id="artist-login" onClick={GoogleLogin}>
